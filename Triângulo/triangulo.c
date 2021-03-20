86dd8291717852f922d52b8e9dec120f444c67ad
“madalenarb” <mariamadalenarob@tecnico.ulisboa.pt>
2021-03-20 13:07:57 +0000

    new file:   a.out 	new file:   triangulo.c

---
+++ Triângulo/triangulo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int h, i, v;
+
+    printf("Insira a altura do triângulo: ");
+    scanf("%d", &h);
+    printf("Eis o triângulo: \n\n");
+    int b = 2 * h - 1;
+    int c = (h + 1) / 2;
+
+    for ( i = 0; i <= c; i++)
+    {
+        for (v = 0; v <= i; v++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for ( i = c - 1 ; i >= 0; i--)
+    {
+        for (v = 0; v <= i; v++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+}
+
+/*
+ h = 2
+ b = 3
+ c = 2
+ i 0 1 0 
+ v 0 1 
+
+
+*/
